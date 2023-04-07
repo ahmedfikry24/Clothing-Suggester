29e3e61952f502d946ef2bfb633a2d8c7a02908e
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-04-07 05:23:54 +0200

    pick an image from storage

--- app/src/main/java/com/example/clothingsuggester/ui/HomeActivity.kt
+++ app/src/main/java/com/example/clothingsuggester/ui/HomeActivity.kt
@@ -1,12 +1,34 @@
 package com.example.clothingsuggester.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import com.example.clothingsuggester.R
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.clothingsuggester.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    var binding: ActivityHomeBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        initOnClickListeners()
+    }
+
+
+    private fun initOnClickListeners() {
+        val pickImageLauncher = registerForActivityResult(ActivityResultContracts.GetContent()){
+        }
+
+        binding!!.selectClothesButton.setOnClickListener {
+            pickImageLauncher.launch("image/*")
+        }
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 }
