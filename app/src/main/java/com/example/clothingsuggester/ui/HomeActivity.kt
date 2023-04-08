3c5aabfca7b03d17adc002d2081e48f3b3c25984
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-04-08 02:36:51 +0200

    request for permission

--- app/src/main/java/com/example/clothingsuggester/ui/HomeActivity.kt
+++ app/src/main/java/com/example/clothingsuggester/ui/HomeActivity.kt
@@ -1,18 +1,26 @@
 package com.example.clothingsuggester.ui
 
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.isVisible
 import com.example.clothingsuggester.databinding.ActivityHomeBinding
 import com.example.clothingsuggester.utlis.SharedPrefManager
 
 class HomeActivity : AppCompatActivity() {
-    var binding: ActivityHomeBinding? = null
-    var imageNumber: Int = 0
-    val clothesList = mutableListOf<String>()
-    var isSelectedImage = false
+    private var binding: ActivityHomeBinding? = null
+    private var imageNumber: Int = 0
+    private val clothesList = mutableListOf<String>()
+    private var isSelectedImage = false
+
+    //permissions
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
@@ -26,13 +34,22 @@ class HomeActivity : AppCompatActivity() {
 
 
     private fun initOnClickListeners() {
-        val pickImageLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
-            if (it != null)
-                saveClothes(it)
-        }
+        val pickImageLauncher =
+            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) {
+                if (it != null)
+                    saveClothes(it)
+            }
 
         binding!!.selectClothesButton.setOnClickListener {
-            pickImageLauncher.launch("image/*")
+            if (ActivityCompat.checkSelfPermission(
+                    this@HomeActivity,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                pickImageLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+            } else {
+                requestPermission.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
         }
     }
 
@@ -47,28 +64,32 @@ class HomeActivity : AppCompatActivity() {
                 if (loadImageNumber() == 0) imageNumber.toString() else (imageNumber + 1).toString(),
                 image.toString()
             ).apply()
+        SharedPrefManager.isSelectedImage = true
     }
 
     private fun isSelectedImageBefore() {
         isSelectedImage = SharedPrefManager.isSelectedImage!!
         if (isSelectedImage == true) {
 
-            binding?.clothesImage?.isVisible = true
-            binding?.selectTextHint?.isVisible = false
+            binding!!.clothesImage.isVisible = true
+            binding!!.selectTextHint.isVisible = false
 
         } else {
-            binding?.clothesImage?.isVisible = false
-            binding?.selectTextHint?.isVisible = true
+            binding!!.clothesImage.isVisible = false
+            binding!!.selectTextHint.isVisible = true
         }
     }
 
     private fun loadClothes() {
         for (i in 0..loadImageNumber()) {
             val image = SharedPrefManager.grtInit(this@HomeActivity).getString(i.toString(), null)
-            println(image)
             if (!image.isNullOrBlank())
                 clothesList.add(image)
         }
+    }
+
+    private fun getLocationPermission() {
+
     }
 
     override fun onDestroy() {
